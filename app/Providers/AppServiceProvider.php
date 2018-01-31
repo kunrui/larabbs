c901cede8a962ef56fd3e8d48c0492faa204ea94
RUI <946667296@qq.com>
2018-01-31 06:28:47 +0000

    Sidebar links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,11 +12,13 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-	{
-		\App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
-		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+    {
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
+        // Carbon 中文化配置
         \Carbon\Carbon::setLocale('zh');
     }
 
@@ -25,10 +27,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-     public function register()
-     {
-         if (app()->isLocal()) {
-             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
-         }
-     }
+    public function register()
+    {
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
+    }
 }

4cb9b061c856ccba919125e9fbe220ad55eeb296
RUI <946667296@qq.com>
2018-02-03 05:27:57 +0000

    topic index

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,12 @@ $api->version('v1', [
         // 分类列表
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        // 话题列表
+        $api->get('topics', 'TopicsController@index')
+            ->name('api.topics.index');
+        // 用户话题列表
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {

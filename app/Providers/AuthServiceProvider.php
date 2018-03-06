bb3f39caffdc97a94989fa563a4efa7600e2b7c6
RUI <946667296@qq.com>
2018-03-06 10:10:02 +0000

    change jwt to passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,13 +26,36 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-     public function boot()
-     {
-         $this->registerPolicies();
+    // public function boot()
+    // {
+    //     $this->registerPolicies();
+    //
+    //     // Passport 的路由
+    //     Passport::routes();
+    //     // access_token 过期时间
+    //     Passport::tokensExpireIn(Carbon::now()->addDays(15));
+    //     // refreshTokens 过期时间
+    //     Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+    //
+    //     \Horizon::auth(function ($request) {
+    //         // 是否是站长
+    //         return \Auth::user()->hasRole('Founder');
+    //     });
+    // }
+    public function boot()
+    {
+        $this->registerPolicies();
 
-         \Horizon::auth(function ($request) {
-             // 是否是站长
-             return \Auth::user()->hasRole('Founder');
-         });
-     }
+        // Passport 的路由
+        Passport::routes();
+        // access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        // refreshTokens 过期时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
+    }
 }
